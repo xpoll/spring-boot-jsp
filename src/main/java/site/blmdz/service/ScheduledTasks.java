ca4fbd64770365120a5428f2cc2178806edf6b33
yangyz <yangyz@yowits.com>
2016-12-05 14:53:39 +0800

    add scheduled

---
+++ src/main/java/site/blmdz/service/ScheduledTasks.java
@@ -0,0 +1,20 @@
+package site.blmdz.service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+	static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+//	@Scheduled(fixedRate = 2000)
+	@Scheduled(cron = "*/2 * * * * ?")
+	public void test1() {
+		System.out.println("current time is: " + sdf.format(new Date()));
+	}
+	
+}
